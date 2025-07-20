ae90ff3624082dbfedfd076f181c97fb4ca1785c
enzosoaresdev <enzo.ferreira.soares@hotmail.com>
2025-07-20 19:37:19 -0300

    Feat: Estrutura final do código validador de senhas.

---
+++ password_validator.py
@@ -0,0 +1,42 @@
+#Password Validator.
+
+def check_length(password):
+    """Verifica se a senha tem pelo menos 8 caracteres."""
+    if len(password) >= 8:
+        return True
+    return False
+
+def has_uppercase(password):
+    """Verifica se a senha contém pelo menos uma letra maiúscula."""
+    for char in password:
+        if char.isupper():
+            return True
+    return False
+
+def has_lowercase(password):
+    """Verifica se a senha contém pelo menos uma letra minúscula."""
+    for char in password:
+        if char.islower():
+            return True
+    return False
+
+def has_digit(password):
+    """Verifica se a senha contém pelo menos um digito(número)."""
+    for char in password:
+        if char.isdigit():
+            return True
+    return False
+
+def classify_password(password):
+    """Classifica a senha, conforme todas verificações forem verdadeiras."""
+    if(
+        check_length(password) and
+        has_uppercase(password) and
+        has_lowercase(password) and
+        has_digit(password)
+    ):
+        return "Senha Forte!"
+    return "Senha Fraca!"
+
+password = input("Digite sua senha: ")
+print(classify_password(password))
